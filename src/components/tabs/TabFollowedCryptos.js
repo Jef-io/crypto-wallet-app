a433ba576ee4985c3b041c78a18fd091bfa6cbde
Jef Schoolmeester <jef.schoolmeester99@gmail.com>
2021-04-15 20:45:52 +0200

    [FEAT] followed cryptos

---
+++ src/components/tabs/TabFollowedCryptos.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
+import DeleteIcon from '@material-ui/icons/Delete'
+import CancelIcon from '@material-ui/icons/Cancel';
+
+export default function TabCryptos ({
+    cryptosList
+}) {
+
+    const cryptos = cryptosList ? cryptosList : [];
+
+    return (
+        <table className="TabCryptos">
+            <thead>
+                <tr>
+                    <th>Crypto</th>
+                    <th>Cours</th>
+                    <th>24 heures</th>
+                    {/* a voir si quantité ou bool */}
+                    <th>Possédée</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {cryptos.map((crypto) => (
+                    <tr>
+                        <td>{crypto.id}</td>
+                        <td>{crypto.current_price} €</td>
+                        <td>{crypto.market_cap_change_percentage_24h}</td>
+                        <td><CancelIcon fontSize="large" color="secondary"/></td>
+                        {/* { crypto.owned === '0' ?
+                        :
+                            <td>{crypto.owned}</td>
+                        } */}
+                        <td>
+                            <IconButton color="primary" aria-label="shop">
+                                <ShoppingCartIcon fontSize="large"/>
+                            </IconButton>
+                            <IconButton color="secondary" aria-label="delete">
+                                <DeleteIcon fontSize="large"/>
+                            </IconButton>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+}
--- src/pages/Cryptos.js
+++ src/pages/Cryptos.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../css/cryptos.css';
 import Title from '../components/Title';
-import TabCryptos from '../components/tabs/TabCryptos';
+import TabFollowedCryptos from '../components/tabs/TabFollowedCryptos';
 import CustomizedButton from '../components/Button';
+import { Link } from 'react-router-dom';
+import { 
+  getFollowedCryptos
+} from '../utils/followedCryptos'
 
 export default function Cryptos () {
 
+  const [cryptos, setCryptos] = useState([]);
+
+  const fetchCryptos = async () => {
+    const result = await getFollowedCryptos();
+    setCryptos(result);
+}
+
+useEffect(() => {
+    fetchCryptos();
+}, [])
+
+  useEffect(() => {
+
+  })
+
+  
   return (
     <main className="Cryptos">
         <section>
           <Title value="Cryptos" variant="1"/>
           <article>
-            <TabCryptos/>
-            <CustomizedButton value="ajouter une cryptomonnaie" color="blue"/>
+            <TabFollowedCryptos cryptosList={cryptos}/>
+          </article>
+        </section>
+        <section>
+          <article>
+            <Link to={'/addcrypto'}>
+              <CustomizedButton value="ajouter une cryptomonnaie" color="blue"/>
+            </Link>
           </article>
         </section>
     </main>
