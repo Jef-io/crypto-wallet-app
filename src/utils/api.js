29c5514d74401b8ca9d9dcb3290535c9e9892b60
Jef Schoolmeester <jef.schoolmeester99@gmail.com>
2021-04-17 12:55:28 +0200

    [FIX] api timeout 4000ms

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3001/"
+    baseURL: "http://localhost:3001/",
+    timeout: 4000
 })
 
 const getRequest = async (route) => {
