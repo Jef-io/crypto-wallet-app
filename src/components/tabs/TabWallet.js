5b8800278a6920b95db8e1395f2e701482f64722
Jef Schoolmeester <jef.schoolmeester99@gmail.com>
2021-04-17 12:54:06 +0200

    [FEAT-FIX] Tab wallet icons & key prop

--- src/components/tabs/TabWallet.js
+++ src/components/tabs/TabWallet.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import IconButton from '@material-ui/core/IconButton'
-import AddCircleIcon from '@material-ui/icons/AddCircle';
-import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
+import IconButton from '../IconButton';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
 
 export default function TabWallet ({
     wallet
@@ -19,18 +19,18 @@ export default function TabWallet ({
                 </tr>
             </thead>
             <tbody>
-                {wallet.map((crypto) => (
-                    <tr>
+                {wallet.map((crypto, id) => (
+                    <tr key={id}>
                         <td>{crypto.id}</td>
                         <td>{crypto.held}</td>
                         <td>{crypto.current_price} €</td>
                         <td>{crypto.current_price*crypto.held} €</td>
                         <td>
-                            <IconButton color="secondary" aria-label="shop">
-                                <AddCircleIcon fontSize="large"/>
+                            <IconButton color="secondary">
+                                <AddShoppingCartIcon fontSize="large"/>
                             </IconButton>
-                            <IconButton color="primary" aria-label="shop">
-                                <RemoveCircleIcon fontSize="large"/>
+                            <IconButton color="primary">
+                                <MonetizationOnIcon fontSize="large"/>
                             </IconButton>
                         </td>
                     </tr>
